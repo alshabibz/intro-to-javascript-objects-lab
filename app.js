fad625ba14c59f12ccb49392eefed8799e9081de
alshabibz <alshabibz@gmail.com>
2025-04-28 14:14:30 +0300

    Exercise 12

--- app.js
+++ app.js
@@ -168,5 +168,29 @@ Also, log the `game.items` array to confirm that the pokeball quantity is being 
 
 Solve Exercise 11 here:
 */
+game.catchPokemon = (pokemonObj) =>{
+  game.party.push(pokemonObj);
+  const pokeball = game.items.find(item => item.name === "pokeball");
+  if (pokeball) {
+    pokeball.quantity -= 1;
+  }
+};
+const newPokemon = pokemon.find(pokemon => pokemon.name === "Dragonite");
+game.catchPokemon(newPokemon);
+console.log(game.items);
+console.log('===================================')
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
 
+Solve Exercise 12 here:
+*/
+game.gyms.forEach(gym => {
+  if (gym.difficulty < 6) {
+    gym.completed = true;
+  }
+});
+console.log(game.gyms);
+console.log('===================================')
 
